b8cc916354dbabfda74af2b4de3d5911cf19a983
Simon Vestergaard Laursen <svl@gavdilabs.com>
2025-10-08 07:02:30 +0200

    chore: code coverage reports (#62)

--- jest.config.ts
+++ jest.config.ts
@@ -14,18 +14,34 @@ const config: Config = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: false, // ENABLE IF COVERAGE
+  collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+    "!src/**/index.ts",
+    "!src/**/types.ts",
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: ["/node_modules/", "/test/", "/lib/", "/demo/"],
+
+  // Coverage reporters
+  coverageReporters: ["text", "text-summary", "html", "json"],
+
+  // Coverage thresholds
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 65,
+      statements: 65,
+    },
+  },
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
